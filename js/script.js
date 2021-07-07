2f284aee79f36d1d07e7525fd04bd50427ba449e
maxfixe258369 <70097773+maxfixe258369@users.noreply.github.com>
2021-07-07 12:18:52 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-$( document ).ready(function() {
-  $(".list").click(function(){
-    if ($('.show-list').css('display') == 'none'){
-      $(".show-list").css("display", "block")
-    }
-    else {
-      $(".show-list").css("display", "none")
-    }
-  });
-
-  $(".cart").click(function(){
-    if ($('.show-cart').css('display') == 'none'){
-      $(".show-cart").css("display", "block")
-    }
-    else {
-      $(".show-cart").css("display", "none")
-    }
-  });
-});
-
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
 }
@@ -25,6 +5,61 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    const scrollTop = $(window).scrollTop();
+    if (scrollTop == 200) {
+      $(".options-choose-class").addClass("fly");
+      $("html").animate(
+        {
+          scrollTop: $(".section-choose-class").offset().top
+        },
+        500
+      );
+    }
+
+    else if (scrollTop == 1436) {
+      $(".stats-content").addClass("show");
+      $("html").animate(
+        {
+          scrollTop: $(".section-our-stats").offset().top
+        },
+        500
+      );
+    }
+
+    else if (scrollTop == 2565 || scrollTop == 2621 || scrollTop == 2709 ) {
+      $("html").animate(
+        {
+          scrollTop: $(".section-best-instructor").offset().top
+        },
+        500
+      );
+    }
+
+    else if (scrollTop == 3801 ||scrollTop == 3925) {
+      $("html").animate(
+        {
+          scrollTop: $(".section-new-experience").offset().top
+        },
+        500
+      );
+    }
+
+    else if (scrollTop == 4804 || scrollTop == 5145) {
+      $("html").animate(
+        {
+          scrollTop: $(".section-pricing").offset().top
+        },
+        500
+      );
+    }
+  });
+  
+})
+
+
 
 
 
